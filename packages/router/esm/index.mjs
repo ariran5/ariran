ce8335712b1e91b1bd8eaa1cf053e60e842acbcd
Arian <aria-n@mail.ru>
2020-07-08 16:35:00 +0300

    Новый роутер

--- packages/router/esm/index.mjs
+++ packages/router/esm/index.mjs
@@ -1,347 +1,238 @@
-import http2 from 'http2';
-import nodePath from 'path'
+import pkg from 'path-to-regexp'
+import path from 'path'
+import http2 from 'http2'
+import cookie from 'cookie'
+import { URLSearchParams } from 'url'
 
-import extendsStream from './lib/extendsStream.mjs'
-export { getBody } from './lib/getBody.mjs'
 
-// TODO: сделать нейронку сервер пуша, что бы она обучалась отдавать файлы для страниц
-// TODO: Сделать чтоб методы get use... можно было деструктуризировать
+const extended = Symbol('extended')
 
 const {
-    HTTP2_HEADER_PATH,
-    HTTP2_HEADER_METHOD,
-    HTTP_STATUS_NOT_FOUND,
-    HTTP_STATUS_INTERNAL_SERVER_ERROR
-  } = http2.constants;
+  join
+} = path.posix;
 
-const windowsSlashRegex = /\\/g
+const { pathToRegexp } = pkg;
 
-const _host = Symbol('host')
+const {
+  HTTP2_HEADER_METHOD,
+  HTTP2_HEADER_PATH,
+  HTTP2_HEADER_STATUS,
+  HTTP2_HEADER_CONTENT_TYPE
+} = http2.constants;
 
-class Router {
-  restFunctions = []
+export default class Router {
 
-  constructor( options = {}){
-    this.basePath = options.basePath || '/'
+  static methods = ['GET', 'POST', 'PUT', 'DELETE'];
 
-    if (options.restFunctions)
-      this.restFunctions = options.restFunctions
+  #pipelines = {
+    GET: [],
+    POST: [],
+    PUT: [],
+    DELETE: []
+  };
+
+  #globalPipeline = [];
+  #basePath;
+  #inititalStore = {};
+
+  get pipline(){
+    return this.#globalPipeline
   }
 
-  parseArgs = (...fnArgs) => {
-    let [path, callback, ...args] = fnArgs
-
-    if ( path instanceof Function ) {
-      path = '/*';
-      [callback, ...args] = fnArgs
-    }
-    if (typeof path === 'string') {
-      path = nodePath.join(this.basePath, path)
-
-      if (~path.indexOf('*')) {
-        if (path[path.length - 1] !== '*') {
-          path = path.replace('/*', '')
-        }
-      }
-      
-      if (windowsSlashRegex.test(path))
-        path = path.replace(windowsSlashRegex, '/')
-    }
-  
-    return {
-      path,
-      callback,
-      args
-    }
-  }
-  
-
-  // http2 (stream, headers) {
-
-  //   // если передается 1 аргумент то это кто-то передал руками опции
-  //   if (arguments.length == 1) {
-  //     this.parseOptions(stream);
-  //   } else {
-  //     return this.request(stream, headers);
-  //   }
-  // }
-
-  // http1 (request, response) {
-
-  //   // if ( parseInt(request.httpVersion) >= 2 ) return;
-
-  //   // // если передается 1 аргумент то это кто-то передал руками опции
-  //   // if (arguments.length == 1) {
-  //   //   this.parseOptions(request);
-  //   // } else {
-  //   //   const {stream, headers} = http1ToHttp2(request, response);
-  //   //   return this.request(stream, headers);
-  //   // }
-  // }
-
-  set host(value){
-    this[_host] = value
+  get store(){
+    return this.#inititalStore
   }
 
-  get host(){
-    return this[_host]
+  set basePath(value){
+    this.#basePath = value
   }
 
-  request = async (...args) => {
-    let [stream, headers, ...arr] = args
-    stream = extendsStream(stream)
-
-    const newArgs = [stream, headers, ...arr]
-
-    if (this[_host] && headers.host !== this[_host]) {
-      return false
-    }
-
-    for (const route of this.restFunctions) {
-      if ( !stream.writable ) {
-        return false
-      }
-
-      try {
-        const Callback = this.runRoute( newArgs, route );
-
-        if (Callback instanceof Promise)
-          await Callback
-
-      } catch (err) {
-        console.error(err);
-      }
-    }
+  get basePath(){
+    return this.#basePath
   }
 
-  get () {
-    if ( !arguments.length ) {
-      throw new Error('Пустой роут');
-    }
-    const { path, callback, args } = this.parseArgs(...arguments)
+  constructor({
+    basePath = '/',
+  } = {}){
+    this.#basePath = basePath
 
-    this.restFunctions.push({
-      method: "GET",
-      path,
-      callback,
-      args
-    })
-
-    return this;
   }
 
-  post () {
-    if ( !arguments.length ) {
-      throw new Error('Пустой роут');
-    }
-    const {path, callback, args} = this.parseArgs(...arguments)
-
-    this.restFunctions.push({
-      method: "POST",
-      path,
-      callback,
-      args
-    })
-
-    return this;
+  get(){
+    this.setRoute('GET', ...arguments)
+  }
+  post(){
+    this.setRoute('POST', ...arguments)
+  }
+  put(){
+    this.setRoute('PUT', ...arguments)
+  }
+  delete(){
+    this.setRoute('DELETE', ...arguments)
   }
 
-  delete () {
-    if ( !arguments.length ) {
-      throw new Error('Пустой роут');
-    }
-    const {path, callback, args} = this.parseArgs(...arguments)
+  use() {
+    const lastArg = arguments[arguments.length - 1]
+    const preLastArg = arguments[arguments.length - 2] ?? null
 
-    this.restFunctions.push({
-      method: "DELETE",
-      path,
-      callback,
-      args
-    })
-
-    return this;
-  }
-
-  put () {
-    if ( !arguments.length ) {
-      throw new Error('Пустой роут');
-    }
-    const {path, callback, args} = this.parseArgs(...arguments)
-
-    this.restFunctions.push({
-      method: "PUT",
-      path,
-      callback,
-      args
-    })
-
-    return this;
-  }
-
-  use () {
-    if ( !arguments.length ) {
-      throw new Error('Пустой роут');
-    }
-    const {path, callback, args} = this.parseArgs(...arguments)    
-
-    if (path && !callback) {
-      // вложенные вызовы через use
-
-      const newRouter = new Router({
-        basePath: path,
-        restFunctions: this.restFunctions
-      })
-      return newRouter
+    if (lastArg instanceof Router) {
+      this.useWithRouter(preLastArg, lastArg)
     } else {
+      this.setRoute(null, preLastArg, lastArg)
+    }
 
-      this.restFunctions.push({
-        method: "USE",
-        path,
-        callback,
-        args
+  }
+
+  useWithRouter(path, routerObject){
+    this.#globalPipeline.push(routerObject)
+
+      Router.methods.forEach(item => {
+        this.#pipelines[item].push(routerObject)
       })
-      return this;
-    }
+
+      routerObject.basePath = join(this.#basePath, path || '(.*)')
   }
 
-  parseCookie = (stream, cookiesString = '') => {
-    if (!cookiesString) {
-      return {}
+  setRoute(method, path, callback){
+
+    if (path instanceof Function) {
+      callback = path
+      path = null
     }
-
-    const cookies =  Object.fromEntries(
-      cookiesString.split(/; */).map(cookie => {
-        return cookie.split('=')
-      })
-    )
-
-    return new Proxy(cookies, {
-      set(obj, key, value) {
-        
-        // console.log(stream.endAfterHeaders);
-        
-        // if (stream.endAfterHeaders) {
-        //   return false
-        // }
-        
-        Reflect.set(obj, key, value)
-        stream.additionalHeaders({
-          'set-cookie': `${key}=${value}`
-        })
-
-        return true
-      }
-    })
-  }
-
-  parsePath = (routePath, reqPathWithQuery) => {
-    const [reqPath, query = ''] = reqPathWithQuery.split('?')
-    let params
-
-    // Если регулярка и путь не прошел ее
-    if (routePath instanceof RegExp) {
-      params = reqPath.match(routePath)
-      
-      if (!params) {
-        return false
-      }
-
-    } else {
-      // поиск слов между слешами
-      const regex = /:?[^/]+/gim
-
-      const matchedPath = routePath.split('?')[0].match(regex) || []
-      const matchedReqPath = reqPath.match(regex) || []
-
-      for (let i = 0; i < (matchedPath.length || 1); i++) {
-        const item = matchedPath[i]
-        if (item === '*')
-          break
-
-        if (i === matchedPath.length - 1 && matchedPath.length !== matchedReqPath.length)
-          return false
-
-        if (/:/.test(item))
-          continue
-
-        if (matchedReqPath[i] !== item)
-          return false
-      }
-
-
-      params = this.parseParams(matchedPath, matchedReqPath)
-    }
-
-    return {
-      params,
-      query: this.parseQuery(query)
-    }
-  }
-
-  runRoute = (reqArgs, {path, method, callback, args}) => {
-
-    const [stream, headers] = reqArgs
-
-    const reqPath = headers[HTTP2_HEADER_PATH]
-    const reqMethod = headers[HTTP2_HEADER_METHOD]
-
-    if ( method !== reqMethod && method !== "USE") {
-      return;
-    }
-
-    const parsedPath = this.parsePath(path, reqPath)
-
-    if (parsedPath) {
-      
-      Object.assign(
-        stream.store,
-        parsedPath,
-        {
-          cookie: this.parseCookie(stream, headers['cookie'])
-        }
-      )
-
-      const Callback = callback(stream, headers, ...args);
-       return Callback;
-
-    }
-
-  }
-
-  parseParams = (matchedPath, matchedReqPath) => Object.fromEntries(
-    matchedPath
-      .map((item, index) => [item, matchedReqPath[index]])
-      .filter(([item]) => /:/.test(item))
-      .map(item => {
-        item[0] = item[0].slice(1)
-        return item
-      })
-  )
-
-  parseQuery = queryString => {
-    const query = queryString
-      .split('&')
-      .map(item => item.split('='))
-      .filter(([key]) => key)
-      .reduce((acc, [key, value = true]) => {
-        acc[key] = value
-        return acc
-      }, {})
     
-    Object.defineProperty(query, 'toString', {
-      value(){
-        return Object.entries(this)
-          .map(item => item.filter(item => !(typeof item === 'boolean')))
-          .map(item => item.join('='))
-          .join('&')
-      },
-      enumerable: false,
-      writable: true,
-      configurable: true,
-    })
-    return query
+    const route = {
+      method,
+      path: null,
+      regex: null,
+      callback,
+    }
+
+    if (!path) {
+      route.path = join(this.#basePath, '(.*)')
+    } else {
+      route.path = join(this.#basePath, path)
+    }
+    
+    route.regex = pathToRegexp(route.path)
+
+    this.#globalPipeline.push(route)
+
+    if (!method) {
+      Router.methods.forEach(item => {
+        this.#pipelines[item].push(route)
+      })
+    } else {
+      this.#pipelines[method].push(route)
+    }
   }
 
+  async run(stream, headers, flags, store = {}){
+    const method = headers[HTTP2_HEADER_METHOD]
+    const path = headers[HTTP2_HEADER_PATH]
+
+    if (!store.searchParams) {
+      const search = path.match(/\?.+/i)[0]
+      store.query = new URLSearchParams(search)
+    }
+
+    if (!Router.methods.includes(method)) {
+      stream.respond({
+        [HTTP2_HEADER_STATUS]: 405,
+      }, {
+        endStream: true,
+      })
+      return
+    }
+
+    if (!stream[extended]) {
+      const end = stream.end.bind(stream)
+      Object.assign(stream, {
+        get [extended](){
+          return true
+        },
+  
+        end() {
+          if (!arguments[arguments.length - 1] instanceof Function) {  
+            return end(...arguments)
+          }
+
+          return new Promise((res, rej) => {
+            end(...arguments, err => {
+              if (err) {
+                rej(err)
+                return
+              }
+              res()
+            })
+          })
+        }
+      })
+    }
+
+    
+    Object.assign(store, this.#inititalStore)
+
+    const pipelinesForThisMethod = this.#pipelines[method]
+
+    for (let i = 0; i < pipelinesForThisMethod.length; i++) {
+      if (stream.destroyed || stream.closed || !stream.writable) {
+        break
+      }
+
+      const item = pipelinesForThisMethod[i]
+
+      if (item instanceof Router) {
+        await item.run(stream, headers, flags, store)
+      } else if (item.regex.test(path)) {
+        const result = item.callback(stream, headers, store, flags)
+        if (result instanceof Promise) {
+          await result
+        }
+      }
+    } 
+  }
 }
-export default Router
-export const router = new Router();
+
+const extendsMethods = {
+  parseCookie(str){
+    return cookie.parse(srt)
+  },
+  toCookie(key, value, options){
+
+    return {
+      toString(){
+        return 
+      }
+
+    }
+
+  },
+  enableSearchParams(bool){
+
+  },
+  getBody(){
+
+  },
+  end(){
+
+  }
+}
+
+
+// function autoclear(options){
+//   const obj = {}
+//   const cache = {}
+//   //const count = 
+
+//   return new Proxy({}, {
+//     set(obj, key, value, receiver){
+//       clearTimeout(cache[key])
+//       Reflect.set(obj, key, value, receiver)
+
+//       cache[key] = setTimeout(() => {
+//         Reflect.deleteProperty(obj, key)
+//         Reflect.deleteProperty(cache, key)
+//       }, 5000)
+//     }
+//   })
+// }
